d0fa80893f76c115d935cc67d86fefb3bd5feaad
Vlad <uladzislaubayouski@gmail.com>
2018-08-27 00:58:28 -0700

    Adding 8-bit code.

--- temperature_8bit.py
+++ temperature_8bit.py
@@ -7,7 +7,7 @@ import Adafruit_DHT
 from RPLCD import CharLCD
 
 GPIO.cleanup()
-lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
+lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
 
 while True:
 
